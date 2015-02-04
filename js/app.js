e2e4366cabe6c91eabe087ccbca382fcceb3fa63
Nanoman <nanoman689@gmail.com>
2015-02-04 07:46:43 -0800

    Almost done
    -Finished up everything expect the .keydown() part
    -Getting the sound to play still has some issues.

--- js/app.js
+++ js/app.js
@@ -3,19 +3,40 @@ $(document).ready(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
-	.mouseleave(function() {
-	    $('.ryu-ready').hide();
-	    $('.ryu-still').show();
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
   })
-	.mousedown(function() {
-    // play hadouken sound
-	    $('.ryu-ready').hide();
-	    $('.ryu-throwing').show();
-	    $('.hadouken').show();
-	    // animate hadouken to the right of the screen
+  .mousedown(function() {
+    /* playHadouken(); */
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '722px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '522px');
+      }
+    );
   })
- 	.mouseup(function() {
-	 	$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  })
+  .keydown(function() {
+    alert("keydown...");
+  })
 });
-});
+
+/*
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+*/
+
