8d4aaed23b64ef32e7de3d9a909a96f7370ab930
Nanoman <nanoman689@gmail.com>
2015-02-05 09:29:03 -0800

    Final touches
    - Got the x key working.
    - Added some instructions.
    - Removed the red box around everything.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
+    $('.ryu-cool').hide();
     $('.ryu-ready').show();
   })
   .mouseleave(function() {
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
     $('.ryu-still').show();
   })
   .mousedown(function() {
-    /* playHadouken(); */
+    playHadouken();
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
     .animate(
@@ -23,20 +26,20 @@ $(document).ready(function() {
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
     $('.ryu-ready').show();
   })
-  .keydown(function() {
-    alert("keydown...");
+  $('body').keydown(function(event) {
+    /* alert(event.which); */
+      if (event.which === 88) {
+        $('.ryu-ready').hide();
+        $('.ryu-cool').show();
+    }  
   })
 });
-
-/*
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
-
-*/
-
